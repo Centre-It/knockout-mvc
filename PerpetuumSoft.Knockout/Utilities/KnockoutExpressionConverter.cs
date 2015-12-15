34908305a48c8a7bae2e763918a867265b430463
Тимур Гимранов <Timur.Gimranov@centre-it.com>
2015-12-15 12:49:17 +0500

    Теперь не убираются скобки, когда в биндинг проверяется длина массива

--- PerpetuumSoft.Knockout/Utilities/KnockoutExpressionConverter.cs
+++ PerpetuumSoft.Knockout/Utilities/KnockoutExpressionConverter.cs
@@ -152,10 +152,13 @@ namespace PerpetuumSoft.Knockout
       if ((member == "Length" || member == "Count") && !data.InstanceNames.Contains(own))
         member = "length";
 
-		// there should not be brackets in the middle of the call chain. KO.Mapping does not generate observables in the middle
-	    own = own.TrimEnd('(', ')');
+        // there should not be brackets in the middle of the call chain. KO.Mapping does not generate observables in the middle
+        if (member != "length")
+        {
+            own = own.TrimEnd('(', ')');
+        }
 
-      string prefix = own == "" ? "" : own + ".";
+        string prefix = own == "" ? "" : own + ".";
       string suffix = member == "length" ? "" : "()";
       string result = prefix + member + suffix;
       if (data.Aliases.ContainsKey(result))
